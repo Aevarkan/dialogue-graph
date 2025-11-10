065a56ea2d2ac199303294fb7b35794db27a7d30
Aevarkan <aevarkan@gmail.com>
2025-11-10 16:37:31 +0800

    feat(app): extend `Button` and `Scene` types to include identifying information

---
+++ packages/web-app/src/types.ts
@@ -0,0 +1,30 @@
+// Copyright (c) 2025 Aevarkan
+// Licensed under the GPLv3 license
+
+import type { Button, Scene } from "@workspace/common";
+
+export interface VisualButton extends Button {
+  /**
+   * Uuid for the button, used for VueFlow.
+   */
+  id: string
+  /**
+   * The identifier of the button's parent scene.
+   * 
+   * @remarks
+   * Is the `scene_tag` in JSON definition.
+   */
+  parentSceneId: string
+}
+
+export interface VisualScene extends Omit<Scene, "buttons"> {
+  /**
+   * Buttons displayed on the dialogue screen.
+   * 
+   * @remarks
+   * Contains extra identification information.
+   * 
+   * There is a maximum of `6` buttons per scene.
+   */
+  buttons: VisualButton[]
+}
