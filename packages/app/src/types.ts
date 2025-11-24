44aa02d7e132acca659346ee0c7c1d67e5ad7018
Aevarkan <aevarkan@gmail.com>
2025-11-24 13:10:12 +0800

    refactor(app): change slot and scene node types

--- packages/app/src/types.ts
+++ packages/app/src/types.ts
@@ -3,33 +3,59 @@
 
 import type { Button, Scene } from "@workspace/common";
 
-export interface VisualButton extends Button {
+/**
+ * Possible child nodes a visual scene can have.
+ */
+export type SceneFunctionSlot = SceneCommandSlot | SceneButtonSlot
+
+/**
+ * Possible button slots a visual scene can have.
+ * 
+ * @todo
+ * This is a hardcoded 6 button limit.
+ * Ensure this is kept up to date.
+ */
+export type SceneButtonSlot = "1" | "2" | "3" | "4" | "5" | "6"
+
+/**
+ * Open and close commands.
+ */
+export type SceneCommandSlot = "open" | "close"
+
+export interface VisualScene extends Scene {
   /**
-   * Uuid for the button, used for VueFlow.
+   * Map of slots to their VueFlow node ids.
+   */
+  commandMap: Map<SceneCommandSlots, string>
+}
+
+export interface VisualSceneCommand {
+  /**
+   * Unique identifier.
    */
   id: string
   /**
-   * The identifier of the button's parent scene.
+   * Whether the command runs on scene open or scene close.
+   */
+  type: SceneCommandSlot,
+  /**
+   * The identifier of the slot's parent scene.
    * 
    * @remarks
    * Is the `scene_tag` in JSON definition.
    */
   parentSceneId: string
-}
-
-export interface VisualScene extends Omit<Scene, "buttons"> {
   /**
-   * Buttons displayed on the dialogue screen.
-   * 
-   * @remarks
-   * Contains extra identification information.
-   * 
-   * There is a maximum of `6` buttons per scene.
+   * The commands that the NPC will run on scene open/close.
    */
-  buttons: VisualButton[]
+  commands: string[]
 }
 
 export interface VisualSlot {
+  /**
+   * Uuid of this slot.
+   */
+  id: string
   /**
    * The identifier of the slot's parent scene.
    * 
